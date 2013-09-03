fb56b9542464737e92efae1ebd9033a6df4a6bb2
Rafe Colton <r.colton@modcloth.com>
2013-09-03 14:30:28 -0400

    Lots of changes
    Long commit, sorry - didn't commit along the way and stuff got tangled.
    Shame on me.
    
    Changes:
    - moved the delivery-handling function (i.e. the business logic) out of main
    - removed rmq log queue following feature. this can be achieved with
      already-existing functionality and the previous implementation wasn't
    very flexible.
    - allowing consume-cat to exit after processing all messages in its
      queues
    - acking messages *after* they are successfully processed

--- amqp-consume-cat/main.go
+++ amqp-consume-cat/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"path"
-	"strings"
 )
 
 import (
@@ -31,84 +28,16 @@ const (
 )
 
 var (
-	queueBindings QueueBindings
-	revFlag       = false
-	versionFlag   = false
-)
-
-type deliveryPlus struct {
-	RawDelivery  amqp.Delivery
-	BodyAsString string
-}
-
 	uriFlag     = flag.String("U", "amqp://guest:guest@localhost:5672", "AMQP Connection URI")
 	rmqLogsFlag = flag.Bool("rabbitmq.logs", false, "Consume from amq.rabbitmq.logs and amq.rabbitmq.trace")
 	showCatFlag = flag.Bool("mrow", false, "")
 	versionFlag = flag.Bool("version", false, "Print version and exit")
 	revFlag     = flag.Bool("rev", false, "Print git revision and exit")
+	quit        = make(chan bool)
 
+	queueBindings QueueBindings
 	debugger      Debugger
 )
-
-func deliver(delivery amqp.Delivery) {
-	if len(*outDirFlag) == 0 {
-		fmt.Printf("%s: %s", delivery.Exchange, string(delivery.Body))
-	} else {
-		deliveryPlus := &deliveryPlus{
-			delivery,
-			string(delivery.Body),
-		}
-		jsonBytes, err := json.MarshalIndent(deliveryPlus, "\t", "\t")
-		if debugger.WithError(err, "Unable to marshall delivery into JSON.") {
-			return
-		}
-
-		var folderName string
-		if len(delivery.MessageId) > 0 {
-			folderName = delivery.MessageId
-		} else {
-			h := sha1.New()
-			fmt.Fprintf(h, "%s", jsonBytes)
-			folderName = fmt.Sprintf("%x", h.Sum(nil))
-		}
-
-		var exchangeStr string
-		if len(delivery.Exchange) == 0 {
-			exchangeStr = "_"
-		} else {
-			exchangeStr = delivery.Exchange
-		}
-
-		pathParts := []string{
-			strings.TrimRight(*outDirFlag, string(os.PathSeparator)),
-			exchangeStr,
-			folderName,
-		}
-		fullPath := strings.Join(pathParts, string(os.PathSeparator))
-
-		fileName := fmt.Sprintf("%s%smessage.json", fullPath, string(os.PathSeparator))
-
-		err = os.MkdirAll(fullPath, os.ModeDir|os.ModePerm)
-		if debugger.WithError(err, fmt.Sprintf("Unable to create output directory '%s'.", fullPath)) {
-			return
-		}
-
-		file, err := os.Create(fileName)
-		if debugger.WithError(err, fmt.Sprintf("Unable to create file '%s'.", fileName)) {
-			return
-		}
-
-		_, err = file.Write(jsonBytes)
-		if debugger.WithError(err, fmt.Sprintf("Unable to write data into buffer for '%s'.", fileName)) {
-			return
-		} else {
-			debugger.Print(fmt.Sprintf("Data written to %s", fileName))
-		}
-
-		err = file.Close()
-		debugger.WithError(err, fmt.Sprintf("Unable to close file '%s'.", fileName))
-	}
-}
 
 func init() {
 	flag.Var(&queueBindings, "q", "Queue bindings specified as \"/\"-delimited strings of the form \"exchange/queue-name/routing-key\"")
@@ -140,38 +69,27 @@ func main() {
 		os.Exit(0)
 	}
 
-	debugger.SetDebug(*debugFlag)
+	deliveries := make(chan interface{})
 
-	quit := make(chan bool)
-	deliveries := make(chan amqp.Delivery)
-
-	if *rmqLogsFlag || len(queueBindings) > 0 {
-		if *rmqLogsFlag {
-			debugger.Print("Tailing RabbitMQ Logs")
-
-			go TailRabbitLogs(*uriFlag, deliveries, debugger)
-
-			go func() {
-				for delivery := range deliveries {
-					deliver(delivery)
-				}
-			}()
+	if len(queueBindings) > 0 {
+		for _, binding := range queueBindings {
+			debugger.Print(fmt.Sprintf("Binding to %s", binding))
 		}
-		if len(queueBindings) > 0 {
-			for _, binding := range queueBindings {
-				debugger.Print(fmt.Sprintf("Binding to %s", binding))
+
+		go ConsumeForBindings(*uriFlag, queueBindings, deliveries, debugger)
+
+		go func() {
+			for delivery := range deliveries {
+				switch delivery.(type) {
+				case nil:
+					quit <- true
+				default:
+					HandleDelivery(delivery.(amqp.Delivery), debugger)
+				}
 			}
-			go ConsumeForBindings(*uriFlag, queueBindings, deliveries, debugger)
-
-			go func() {
-				for delivery := range deliveries {
-					deliver(delivery)
-				}
-			}()
-		}
+		}()
 	} else {
-		fmt.Println("ERROR: You must either consume rabbitmq logs or " +
-			"define at least one exchange/queue/binding argument.")
+		fmt.Println("ERROR: define at least one exchange/queue/binding argument.")
 		flag.Usage()
 		os.Exit(NOT_COOL_ZEUS)
 	}
--- consuming.go
+++ consuming.go
@@ -1,15 +1,29 @@
 package amqptools
 
 import (
+	"crypto/sha1"
+	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 import (
 	"github.com/streadway/amqp"
 )
+
+var (
+	outDirFlag        = flag.String("d", "", "Output directory for messages. If not specified, output will go to stdout.")
+	continuousConsume = flag.Bool("continuous", false, "If true, consume indefinitely ; otherwise, exit when queue is emptied.")
+	consumerTag       string
+)
+
+/*
+ESTABLISHING BINDING LIST
+*/
 
 type QueueBinding struct {
 	QueueName  string
@@ -60,121 +74,173 @@ func (qb *QueueBindings) Set(value string) error {
 	return nil
 }
 
+/*
+ESTABLISHING CONNECTIONS
+*/
+
 type consumerChannel struct {
 	deliveries <-chan amqp.Delivery
 	binding    *QueueBinding
 }
 
-func ConsumeForBindings(connectionUri string, bindings QueueBindings,
-	deliveries chan amqp.Delivery, debugger *Debugger) {
-
+func ConsumeForBindings(connectionUri string, bindings QueueBindings, deliveries chan interface{}, debugger Debugger) {
 	defer close(deliveries)
 
+	// establish connection
 	conn, err := amqp.Dial(connectionUri)
-	if debugger.WithError(err, "connection.open:", err) {
-		return
+	if debugger.WithError(err, "connection.establish: ", err) {
+		os.Exit(1)
 	}
-
 	defer conn.Close()
+	debugger.Print("Connection established")
 
+	// open channel
 	channel, err := conn.Channel()
-	if debugger.WithError(err, "connection.open:", err) {
-		return
+	if debugger.WithError(err, "channel.open: ", err) {
+		os.Exit(2)
 	}
+	debugger.Print("Channel opened")
+
+	// set qos
+	qosValue := 10
+	err = channel.Qos(qosValue, 0, false)
+	if debugger.WithError(err, "channel.qos: ", err) {
+		os.Exit(3)
+	}
+	debugger.Print(fmt.Sprintf("Channel QOS set to %d", qosValue))
 
 	var consumerChannels []consumerChannel
 
-	for _, binding := range bindings {
-		queue, err := channel.QueueDeclare(binding.QueueName, false, true, false, false, nil)
-		if debugger.WithError(err, "channel.queue_declare:", err) {
-			return
-		}
-
-		err = channel.QueueBind(queue.Name, binding.RoutingKey, binding.Exchange, false, nil)
-		if debugger.WithError(err, "channel.queuebind", err) {
-			return
-		}
-
-		consumerChan, err := channel.Consume(queue.Name, "", true, false, false, false, nil)
-		if debugger.WithError(err, "channel.consume", err) {
-			return
-		}
-
-		consumerChannels = append(consumerChannels, consumerChannel{consumerChan, binding})
+	uuidBytes, err := exec.Command("uuidgen").Output()
+	if debugger.WithError(err, "uuidgen", err) {
+		os.Exit(4)
 	}
 
-	for {
+	consumerTag = string(uuidBytes)
+
+	for _, binding := range bindings {
+		err = channel.QueueBind(binding.QueueName, binding.RoutingKey, binding.Exchange, false, nil)
+		if debugger.WithError(err, "channel.queuebind ", err) {
+			return
+		}
+
+		if *continuousConsume {
+			//handle acking after writing file
+			/*
+				autoAck = false (must manually Ack)
+				exclusive = true (so we only try to read from one consumer at a time)
+				noLocal = true (so that if this consumer republishes to the failure queue, it will not pick the messages back up)
+				noWait = true
+			*/
+			consumerChan, err := channel.Consume(binding.QueueName, consumerTag, false, true, true, true, nil)
+			if debugger.WithError(err, "channel.consume ", err) {
+				return
+			}
+
+			consumerChannels = append(consumerChannels, consumerChannel{consumerChan, binding})
+		} else {
+			consumerChannels = append(consumerChannels, consumerChannel{nil, binding})
+		}
+	}
+
+	if *continuousConsume {
+		for {
+			for _, ch := range consumerChannels {
+				select {
+				case delivery := <-ch.deliveries:
+					deliveries <- delivery
+				}
+			}
+		}
+	} else {
+		count := 0
 		for _, ch := range consumerChannels {
-			select {
-			case delivery := <-ch.deliveries:
-				deliveries <- delivery
+			binding := ch.binding
+			debugger.Print("Getting messages from queue", binding.QueueName)
+			for {
+				delivery, ok, err := channel.Get(binding.QueueName, false)
+				if debugger.WithError(err, "channel.get ", err) {
+					os.Exit(5)
+				}
+				if ok == false {
+					deliveries <- nil
+				} else {
+					count++
+					fmt.Printf("count: %d\n", count)
+					deliveries <- delivery
+				}
 			}
 		}
 	}
 }
 
-func TailRabbitLogs(connectionUri string, deliveries chan amqp.Delivery, debugger *Debugger) {
+/*
+HANDLING MESSAGES
+*/
 
-	defer close(deliveries)
+type deliveryPlus struct {
+	RawDelivery  amqp.Delivery
+	BodyAsString string
+}
 
-	conn, err := amqp.Dial(connectionUri)
-	if debugger.WithError(err, "connection.open:", err) {
-		return
-	}
-
-	defer conn.Close()
-
-	channel, err := conn.Channel()
-	if debugger.WithError(err, "channel.open:", err) {
-		return
-	}
-
-	queue, err := channel.QueueDeclare("", false, true, false, false, nil)
-	if debugger.WithError(err, "channel.queue_declare:", err) {
-		return
-	}
-
-	err = channel.QueueBind(queue.Name, "#", "amq.rabbitmq.log", false, nil)
-	if debugger.WithError(err, "channel.queue_bind:", err) {
-		return
-	}
-
-	logs, err := channel.Consume(queue.Name, "firehose-log-consumer",
-		true, false, false, false, nil)
-	if debugger.WithError(err, "channel.consume", err) {
-		return
-	}
-
-	debugger.Print("Consuming messages from amq.rabbitmq.log")
-
-	err = channel.QueueBind(queue.Name, "#", "amq.rabbitmq.trace", false, nil)
-	if debugger.WithError(err, "channel.queue_bind:", err) {
-		return
-	}
-
-	traces, err := channel.Consume(queue.Name, "firehose-trace-consumer",
-		true, false, false, false, nil)
-	if debugger.WithError(err, "channel.consume:", err) {
-		return
-	}
-
-	debugger.Print("Consuming messages from amq.rabbitmq.trace")
-
-	channelCloses := channel.NotifyClose(make(chan *amqp.Error))
-	connCloses := conn.NotifyClose(make(chan *amqp.Error))
-
-	for {
-		select {
-		case entry := <-logs:
-			deliveries <- entry
-		case entry := <-traces:
-			deliveries <- entry
-		case err := <-connCloses:
-			debugger.Print("received Connection close error:", err)
-			return
-		case err := <-channelCloses:
-			debugger.Print("received channel close error:", err)
+func HandleDelivery(delivery amqp.Delivery, debugger Debugger) {
+	if len(*outDirFlag) == 0 {
+		fmt.Println("got here")
+		//fmt.Printf("%s: %s", delivery.Exchange, string(delivery.Body))
+	} else {
+		deliveryPlus := &deliveryPlus{
+			delivery,
+			string(delivery.Body),
+		}
+		jsonBytes, err := json.MarshalIndent(deliveryPlus, "\t", "\t")
+		if debugger.WithError(err, "Unable to marshall delivery into JSON.") {
 			return
 		}
+
+		var folderName string
+		if len(delivery.MessageId) > 0 {
+			folderName = delivery.MessageId
+		} else {
+			h := sha1.New()
+			fmt.Fprintf(h, "%s", jsonBytes)
+			folderName = fmt.Sprintf("%x", h.Sum(nil))
+		}
+
+		var exchangeStr string
+		if len(delivery.Exchange) == 0 {
+			exchangeStr = "_"
+		} else {
+			exchangeStr = delivery.Exchange
+		}
+
+		pathParts := []string{
+			strings.TrimRight(*outDirFlag, string(os.PathSeparator)),
+			exchangeStr,
+			folderName,
+		}
+		fullPath := strings.Join(pathParts, string(os.PathSeparator))
+
+		fileName := fmt.Sprintf("%s%smessage.json", fullPath, string(os.PathSeparator))
+
+		err = os.MkdirAll(fullPath, os.ModeDir|os.ModePerm)
+		if debugger.WithError(err, fmt.Sprintf("Unable to create output directory '%s'.", fullPath)) {
+			return
+		}
+
+		file, err := os.Create(fileName)
+		if debugger.WithError(err, fmt.Sprintf("Unable to create file '%s'.", fileName)) {
+			return
+		}
+
+		_, err = file.Write(jsonBytes)
+		if debugger.WithError(err, fmt.Sprintf("Unable to write data into buffer for '%s'.", fileName)) {
+			return
+		} else {
+			debugger.Print(fmt.Sprintf("Data written to %s", fileName))
+		}
+
+		err = file.Close()
+		debugger.WithError(err, fmt.Sprintf("Unable to close file '%s'.", fileName))
 	}
+	delivery.Ack(false)
 }
